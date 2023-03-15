8d5dedcaaabd94fa46c148604d1906c3ffc0c3e9
Osvaldo Junior <osvaldo.rodrigues@acad.pucrs.br>
2023-03-15 14:05:21 -0300

    15-03-23-1hrs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
+import './global.css';
+
+import { Header } from './components/Header';
+import { Post } from './components/Post';
+import { Sidebar } from './components/Sidebar';
+import styles from './App.module.css';
+
 export function App() {
   return (
     <div>
-      <h1>
-        First Commit
-      </h1>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <Post 
+            author="Osvaldo Júnior"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis sint veniam at alias quod repellendus. Voluptas id, nulla ipsa quas doloribus quia enim aliquam, fugit aliquid nihil, explicabo eligendi delectus?"
+          />
+
+          <Post 
+            author="Gisele Taborda"
+            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis sint veniam at alias quod repellendus. Voluptas id, nulla ipsa quas doloribus quia enim aliquam, fugit aliquid nihil, explicabo eligendi delectus?"
+          />
+        </main>
+      </div>
     </div>
   )
 }
+
