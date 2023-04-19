1470fea3c15493d4ed0af5cfdb322d4a79a57241
0svaldojunior <app.o.r.jr@gmail.com>
2023-04-19 14:26:16 -0300

    Finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,60 @@ import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 import styles from './App.module.css';
 
+// author: {avatarURL: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarURL: "https://github.com/0svaldojunior.png",
+      name: "Osvaldo Junior",
+      role: "CTO @ Buyee.me",
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋'},
+      {type: 'paragraph', content: 'Acabei de subir mai,s um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-04-19 10:35:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarURL: "https://github.com/giselek.png",
+      name: "Gisele Knack",
+      role: "CEO @ Buyee.me",
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala pessoal 👋'},
+      {type: 'paragraph', content: 'Subir um projeto novo em meu portifa. 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-04-03 21:43:00'),
+  }
+]
+
 export function App() {
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
         <Sidebar />
-
         <main>
-          <Post 
-            author="Osvaldo Júnior"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis sint veniam at alias quod repellendus. Voluptas id, nulla ipsa quas doloribus quia enim aliquam, fugit aliquid nihil, explicabo eligendi delectus?"
-          />
-
-          <Post 
-            author="Gisele Taborda"
-            content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Veritatis sint veniam at alias quod repellendus. Voluptas id, nulla ipsa quas doloribus quia enim aliquam, fugit aliquid nihil, explicabo eligendi delectus?"
-          />
+          {
+            posts.map(post => {
+              return (
+                <Post 
+                  key={post.id}
+                  author={post.author}
+                  content={post.content}
+                  publishedAt={post.publishedAt}
+                />
+              );
+            })
+          }
         </main>
       </div>
     </div>
